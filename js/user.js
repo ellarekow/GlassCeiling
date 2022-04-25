b5c0d3e56908acf4d3cd2d11904b9ef2d0914f19
errekow <errekow@iastate.edu>
2022-04-25 08:17:05 -0500

    Users file in place for database for now

---
+++ js/user.js
@@ -0,0 +1,8 @@
+class User {
+    constructor(uName, pic, email, password) {
+        this.uName = uName;
+        this.pic = pic;
+        this.email = email;
+        this.password = password;
+    }
+}
