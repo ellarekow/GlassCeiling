bc53225ebdc5a37b6ad52df6dd668a890625e3b0
khawken10 <khawken10@hotmail.com>
2022-04-27 15:54:58 -0500

    Reformatted profile page

--- js/SelectProfilePic.js
+++ js/SelectProfilePic.js
@@ -1,33 +1,33 @@
 
 let blank = document.createElement("img");
 blank.src="images/blank-profile-photo.jpeg";
-blank.width="100";
-blank.height="100";
+blank.width="200";
+blank.height="200";
 
 let kiwi = document.createElement("img");
 kiwi.src="images/kiwi.png";
-kiwi.width="100";
-kiwi.height="100";
+kiwi.width="200";
+kiwi.height="200";
 
 let plum = document.createElement("img");
 plum.src="images/plum.png";
-plum.width="100";
-plum.height="100";
+plum.width="200";
+plum.height="200";
 
 let blueberry = document.createElement("img");
 blueberry.src="images/blueberry.png";
-blueberry.width="100";
-blueberry.height="100";
+blueberry.width="200";
+blueberry.height="200";
 
 let pear = document.createElement("img");
 pear.src="images/pear.png";
-pear.width="100";
-pear.height="100";
+pear.width="200";
+pear.height="200";
 
 let pomegranate = document.createElement("img");
 pomegranate.src="images/pomegranate.png";
-pomegranate.width="100";
-pomegranate.height="100";
+pomegranate.width="200";
+pomegranate.height="200";
 
 let pic=blank;
 
